943b7ade3029f6e7376231dcd58e792b6c3a4cc9
Chunk3r <patrick.pirig@stud.hn.de>
2023-06-07 00:01:31 +0200

    init planerzeugung

---
+++ src/flaskr/dbm.py
@@ -0,0 +1,18 @@
+#imports
+
+#daten der mitarbeiter aus der db importieren
+
+#daten der kunden aus der db importieren
+
+#krankmeldungen aus der db importieren
+
+#besuche aus der db importieren
+
+
+##Tagesplan erstellen##
+
+#ersten pflege-mitarbeiter auswählen, falls Tagesplan bereits voll, nächsten pflege-mitarbeiter auswählen
+
+#ersten kunden auswählen, wenn bereits versorgt oder nicht die richtige pflegekategorie(ambulant/stationär) nächsten kunden auswählen
+
+#neuen besuch-eintrag erstellen, 
