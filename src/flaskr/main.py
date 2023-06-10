512fd6930f4ba762e29cd46579bd3e87dce74a34
mErt <mert.atalay@outlook.de>
2023-06-10 11:31:20 +0200

    Krankschreibungen sind fertig

--- src/flaskr/main.py
+++ src/flaskr/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint,flash, render_template, redirect, url_for, request
 from flaskr.db import get_db
 from flask_login import login_required, current_user
 #app = Flask(__name__)
@@ -24,7 +24,7 @@ def dienstplan():
 
 
 
-@main.route('/verwaltung')
+@main.route('/verwaltung', methods=["GET","POST"])
 @login_required
 def verwaltung():
     cursor = get_db().cursor()
@@ -39,3 +39,64 @@ def verwaltung():
 @login_required
 def krankMB():
     return render_template("krankMB.html")
+
+@main.route('/krankPT')
+@login_required
+def krankPT():
+    return render_template("krankPT.html")
+
+
+
+@main.route('/krankMB', methods=["POST"])
+@login_required
+def krankMB_post():
+    VorName = request.form.get('VorName').strip()
+    NachName = request.form.get('NachName').strip()
+    VonDatum = request.form.get('VonDatum').strip()
+    EndeDatum = request.form.get('EndeDatum').strip()
+    
+    ID_cmd = "SELECT MB_ID FROM Mitarbeiter WHERE VorName='" + VorName + "' AND NachName='" + NachName +  "'; "
+   
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    ID = cursor.execute(ID_cmd).fetchone()
+    if not ID:
+
+        flash("Nutzer nicht gefunden")
+        return redirect(url_for('main.krankMB'))
+
+
+    sql_cmd = "INSERT INTO Dienstbefreiung_Mitarbeiter(Mitarbeiter_ID, Start_Datum, Ende_Datum) VALUES (" + str(ID[0]) + " , '" + VonDatum + "' , '" + EndeDatum + "');"
+    cursor.execute(sql_cmd)
+    conn.commit()
+
+    return redirect(url_for('main.verwaltung'))
+
+@main.route('/krankPT', methods=["POST"])
+@login_required
+def krankPT_post():
+    VorName = request.form.get('VorName').strip()
+    NachName = request.form.get('NachName').strip()
+    VonDatum = request.form.get('VonDatum').strip()
+    EndeDatum = request.form.get('EndeDatum').strip()
+    
+    ID_cmd = "SELECT Kunden_ID FROM Kunde WHERE VorName='" + VorName + "' AND NachName='" + NachName +  "'; "
+
+    conn = get_db()
+    cursor = conn.cursor()
+    ID = cursor.execute(ID_cmd).fetchone()
+    if not ID:
+        flash("Patient nicht gefunden")
+        return redirect(url_for('main.krankPT'))
+
+
+    sql_cmd = "INSERT INTO Krankschreibung_Kunde(Kunden_ID, Start_Datum, Ende_Datum) VALUES (" + str(ID[0]) + " , '" + VonDatum + "' , '" + EndeDatum + "');"
+    
+    cursor.execute(sql_cmd)
+    conn.commit()
+
+
+    return redirect(url_for('main.verwaltung'))
+
