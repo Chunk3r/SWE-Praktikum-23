5bfa84651ff8a5b7cb186449dd25d705bf279a30
Chunk3r <patrick.pirig@stud.hn.de>
2023-06-11 22:04:46 +0200

    POC der Unittests

---
+++ src/flaskr/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from flaskr import create_app
+
+class BlueprintTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app().test_client()
+
+    def test_availability(self):
+        response = self.app.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200, 'test_availability expected stautscode 200 but statuscode is: ' + response.status_code)
+
+    def test_incorrect_login(self):
+        response = self.app.post('/login', data={'NachName': 'keinRichtigerName'})
+        self.assertEqual(response.status_code, 401, 'test_incorrect_login expected statuscode 401 but statuscode is: ' + response.status_code)
+
+    def test_correct_login(self):
+        response = self.app.post('/login', data={'NachName': 'Miller'})
+        self.assertEqual(response.status_code, 200, 'test_correct_login expected statuscode 200 but statuscode is: ' + response.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
